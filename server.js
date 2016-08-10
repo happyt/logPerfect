229eb084bbd1c1a0c306c094c9d986e353da516d
Ian Morton <ian.morton@sky.uk>
2016-08-10 10:42:11 +0100

    studio version Aug 10 2016

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var finalhandler = require('finalhandler')
 var http = require('http')
 var morgan = require('morgan')
+var fs = require('fs')
+var url = require('url')
+var js2xmlparser = require("js2xmlparser")
 
 var PORT = 3000;
 console.log(process.argv);
@@ -17,18 +20,35 @@ if (process.argv.length > 2) {
 }
 
 // create "middleware" 
-var logger = morgan('combined')
+var logger = morgan('combined');
 
 var server = http.createServer(function (req, res) {
-  var done = finalhandler(req, res)
+  var done = finalhandler(req, res);
   logger(req, res, function (err) {
-    if (err) return done(err)
+    if (err) return done(err);
  
+	var url_parts = url.parse(req.url, true)
+	console.log(url_parts)
+	var thejson = url_parts.query.shotdata.toUpperCase()
+	thejson = thejson.replace(/FALSE/g, "false")
+//	delete thejson.startingPosition
+//	delete thejson.finishingPosition
+	
+	console.log(thejson)
+	console.log("===")
+	var thexml = js2xmlparser("SHOT", thejson)
+ 	fs.writeFile("T:/golfFeeds/PPTrackman.xml", thexml, function(err) {
+		if (err) {
+			return console.log(err);
+		}
+	})
     // respond to request 
-    res.setHeader('content-type', 'text/plain')
-    res.end('hello, world!')
+    res.setHeader('content-type', 'text/plain');
+    res.end('writing file');
+	
   })
 })
 
+
 server.listen(PORT)
-console.log(`Server running at ` + PORT);
+console.log("Server listening at " + PORT);
---
+++ test.js
@@ -0,0 +1,3 @@
+var a = {"abc":3, "def": 77, "ghi":99}
+delete a.def
+console.log(a)
